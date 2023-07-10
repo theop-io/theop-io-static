2567c0692304a158ee5116f6c1823df3bd1d0eaa
Robin Giese <robin@grumpycorp.com>
2023-07-09 18:08:08 -0700

    Add initial real Words content

--- src/the-words/the-words-list.ts
+++ src/the-words/the-words-list.ts
@@ -1,9 +1,108 @@
 export const theWordsList: [string, string][] = [
-  ["Blah", "Definition for blah"],
-  ["Stuffy stuff", "Lots of stuff"],
-  ["Word1", "Definition for word 1"],
   [
-    "Word2",
-    "Definition for word 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vehicula id arcu sit amet egestas. Morbi sagittis pretium magna non vestibulum. Ut rutrum nunc ac finibus maximus. Duis magna mauris, iaculis eu velit a, aliquet iaculis urna. Curabitur condimentum non diam at malesuada. Donec ullamcorper ullamcorper ipsum at bibendum. Fusce felis eros, mattis a rhoncus vel, interdum egestas lectus. Vestibulum nisl erat, bibendum sit amet lorem eget, viverra malesuada metus. Donec sit amet laoreet urna, quis tempus odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Mauris sollicitudin eros a arcu tempus ultrices. Suspendisse aliquet erat.",
+    "Solid",
+    "A piece of solid duvatene that light cannot pass through in a frame used to block out light from a scene or take care of lens flare.",
   ],
+
+  [
+    "Flop/Floppy",
+    "A 4x4 solid that has another 4x4 piece of black fabric that is attached on one edge and held in place by velcro on the opposite edge. Once in place, the “flop” part of the solid can be pulled down to either make a right angle or turn the 4x4 into a 4x8.",
+  ],
+
+  [
+    "Apple Box",
+    "A wooden rectangular in various sizes used for all sorts of things from standing on to raising things up. Sizes are as follows: Pancake - 12x20x1, Quarter - 12x20x2, Half - 12x20x4, Full 12x20x8. Apple boxes are called from by name and asking for a complement means bring one of everything. “Not sure what we will need so bring me 2 pancakes, a half and a full and we will see if that works.”",
+  ],
+
+  [
+    "Cup Blocks",
+    "A 6”x6”x2” piece of wood with a slight bowl carved out of the top of it. Cup blocks are used to raise things and since they have the indented bowl, furniture legs will sit in them and not slide off. They can easily be stacked to make things higher.",
+  ],
+
+  [
+    "Wedge",
+    "A 12” wooden 2x4 cut on a diagonal so the one end of it is thin and it gradually increases to the far end. Camera wedges are much smaller versions of this.",
+  ],
+
+  [
+    "Camera Wedge",
+    "A small wooden, plastic or metal wedge used to shim up and level a camera when it is on a surface such as the floor or a sandbag.",
+  ],
+
+  [
+    "Dolly Track",
+    "Metal dolly track that is generally straight and interlocking. The dolly is placed on top of it with special wheels and can ride on the rails like a train. Dolly track can be as long as 100 feet or more but without specialty equipment can only go straight. There are rare occasions where curved track is used but it's not usually done.",
+  ],
+
+  [
+    "Precision Track",
+    "Dolly track that has been machined to extreme tolerances so there are no bumps between the seams in the tracks.",
+  ],
+
+  [
+    "Boards/Planks",
+    "Flat long pieces of wood, usually 1 foot wide, that can quickly be thrown down to achieve a smooth dolly shot. Usually come in 12 or 16’.",
+  ],
+
+  [
+    "Dance Floor",
+    "Flat sheets of wood and plastic, usually 4’x8’ that are built on uneven floor so that the dolly can be moved around smoothly but not in a straight line as if on track.",
+  ],
+
+  [
+    "Dolly Party",
+    "When four or more grips get together to carry a dolly up or down an area where it can’t roll. “Let's get a dolly party over here to get this thing up those stairs.”",
+  ],
+
+  [
+    "Reconnoiter",
+    "Sadly a word used completely incorrectly in the film industry to mean to move something. “Lets reconnoiter this couch over there so we can get the camera in place”. In reality reconnoiter means to scout out ahead of time and gather information. Use this term incorrectly around Simon Jarvis and he will lose his mind.",
+  ],
+
+  ["Follow Focus", "A device the 1st AC uses to change the focus on a lens during the scene."],
+
+  [
+    "Sand Bag",
+    "A heavy bag used to secure stands and such from falling over. Can be used for a million different things and often can make the difference in a shot.",
+  ],
+
+  ["Shot Bag", "A small bag filled with actual metal gun shot, usually ten pounds."],
+
+  ["Ball Buster", "A large sand bag, usually 35 pounds."],
+
+  ["Skoche", "A small amount. “Move that lamp over just a skoche for me.”"],
+
+  [
+    "Clicks",
+    "Denotes time period on a timecard. Most shows break the hour into 6 minute intervals so each hour has 10 intervals or ten clicks. “I put two clicks on your time card because parking is so far away from set.”",
+  ],
+
+  [
+    "Tailights",
+    "The absolute time a crew has to be at a location. This doesn’t mean stopping shooting at that time, it means tail lights are leaving the location. “We are tailights at this location at midnight so we will probably have to finish shooting by 11 to allow the crew to wrap in time.”",
+  ],
+
+  ["Going Again", "Shooting an additional take. “We got that and we are going again.”"],
+
+  [
+    "If it Aint It Aint",
+    "Slang term used to let everyone know that the present shot is the last shot of the day or at that location and anything not working can be packed away for the night/move to next location. “We have one more take of this shot and it’s the last scene here so if it aint it aint.”",
+  ],
+
+  [
+    "Walk Away",
+    "Quite literally, drop what you are doing and leave the vicinity. “We have to get this in the next 5 minutes so if you aren’t involved in this shot, walk away.”",
+  ],
+
+  [
+    "Bogie",
+    "used to describe someone you aren’t supposed to see in the frame, generally during a lockup. “We should cut because I’ve got two bogies across the street pointing at us and taking pictures.”",
+  ],
+
+  [
+    "Upstage/Downstage",
+    "Upstage is away from camera, downstage is towards camera. Easy way to remember it is theater. Down stage is towards the audience and upstage is away from the audience.",
+  ],
+
+  ["Wheels of Death", "Pizza"],
 ];
