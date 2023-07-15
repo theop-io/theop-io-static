bb2ee48f12bd9cb9654c5a7000a510782acc19b1
Robin Giese <robin@grumpycorp.com>
2023-07-15 14:14:08 -0700

    Add support for eslint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+/* eslint-env browser */
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  env: {
+    browser: true,
+  },
+  ignorePatterns: [".eslintrc.js", "prettier.config.js", "webpack.config.js"],
+};
