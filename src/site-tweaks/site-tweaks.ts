2f5192924caac0534f05b46b7675d445fb5f1074
Robin Giese <robin@grumpycorp.com>
2023-07-16 15:57:22 -0700

    Site tweaks: Attempt to fix mobile menu entry coloring

--- src/site-tweaks/site-tweaks.ts
+++ src/site-tweaks/site-tweaks.ts
@@ -51,13 +51,24 @@ allNoUnderlineLinks.forEach((linkElement) => {
 // Site tweak: Apply coloring to navigation bar entries
 //
 
-function styleNavigationElement(element: HTMLLinkElement | HTMLSpanElement) {
+function styleNavigationElement(element: HTMLLinkElement | HTMLSpanElement | HTMLDivElement) {
+  // Find suitable text element to manipulate
   const linkTextElement =
     element.querySelector("div") ?? // Descend into inner <div> if it exists
     element.querySelector("span:not([class])") ?? // Mobile nested menu may be in a class-less span
     element; // Otherwise use this element as-is
 
-  const innerText = linkTextElement.innerText;
+  // Extract HTML because for whatever reason, the mobile menu items don't present a cogent innerText, only innerHTML
+  const innerHTML = linkTextElement.innerHTML;
+
+  if (innerHTML.includes("<")) {
+    // Interior HTML tags - walk away and leave element as-is
+    return;
+  }
+
+  // Remove new-lines and spaces from start and end of string
+  const innerText = innerHTML.replace(/^[\n ]+|[\n ]+$/g, "");
+
   const thePrefix = "The";
 
   if (!innerText.startsWith(thePrefix)) {
@@ -66,7 +77,7 @@ function styleNavigationElement(element: HTMLLinkElement | HTMLSpanElement) {
   }
 
   // Reset element content to just "The" prefix
-  linkTextElement.innerText = thePrefix;
+  linkTextElement.innerText = thePrefix + " ";
 
   // Add span for remaining inner text wrapped in a class'd <span>
   const remainingInnerText = innerText.substring(thePrefix.length);
@@ -94,6 +105,6 @@ function styleNavigationElement(element: HTMLLinkElement | HTMLSpanElement) {
 
   // - Top-level and nested mobile menu links
   document
-    .querySelectorAll<HTMLLinkElement>("div.header-menu-nav-item-content")
+    .querySelectorAll<HTMLDivElement>("div.header-menu-nav-item-content")
     .forEach(styleNavigationElement);
 }
