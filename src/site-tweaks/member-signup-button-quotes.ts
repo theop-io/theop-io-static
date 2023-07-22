ebdec986a37ea91169be9477af8b526acd3e8f49
proteusmd <138808268+proteusmd@users.noreply.github.com>
2023-07-22 05:57:06 -0700

    Update member-signup-button-quotes.ts

--- src/site-tweaks/member-signup-button-quotes.ts
+++ src/site-tweaks/member-signup-button-quotes.ts
@@ -12,7 +12,7 @@ export const memberSignupButtonQuotes = [
   "“I want to be alone, so I can click this button and become a member today.”",
   "“Mama always said that life is like a box of chocolates, you never know what you are gonna get. But if you become a member, you will.”",
   "“Houston, we have a problem. But I'm pretty sure you can fix it by becoming a member today. ”",
-  "“Life moves pretyt fast. If you dont stop and look around once in a while, or at least become a member, you could miss it.”",
+  "“Life moves pretty fast. If you dont stop and look around once in a while, or at least become a member, you could miss it.”",
   "“Carpe Diem. Sieze the day boys. Make your lives extraordinairy. Become a member before time runs out.”",
   "“You haven't become a member yet? Inconceivable!!”",
   "“Allright Mr. Demille. I'm a member now. I'm ready for my closeup.“",
