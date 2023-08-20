77cb01bfd32f83db8603b0bcf3aa204df031f963
Robin Giese <robin@grumpycorp.com>
2023-08-20 13:54:28 -0700

    SiteTweaks: Redirect and retitle primary become-a-member button for logged-in members

--- src/site-tweaks/is-user-logged-in.ts
+++ src/site-tweaks/is-user-logged-in.ts
@@ -2,16 +2,22 @@
 // Infer whether we have a logged-in user
 //
 
-const cookieObject = document.cookie
-  .split(";") // Split cookie string into "key=encodedValue" array
-  .map((kv) => kv.split("=")) // Split each entry into [key, encodedValue]
-  .map((kv) => [kv[0].trim(), decodeURIComponent(kv[1])]) // Remap [key, encodedValue] -> [trimmedKey, decodedValue]
-  .reduce<Record<string, any>>((o, kv) => {
-    // Accumulate list into a single object
-    o[kv[0]] = kv[1];
-    return o;
-  }, {});
+// Cookie processing the cheap and simple way, which is all we should need
+// - Check if a potential Memberspace JWT-style access token is present in our cookies
+const isUserLoggedIn = document.cookie.includes("_ms-access-token");
 
-const isUserLoggedIn = "SiteUserInfo" in cookieObject;
+// Cookie processing the comprehensive way
+// const cookieObject = document.cookie
+//   .split(";") // Split cookie string into "key=encodedValue" array
+//   .map((kv) => kv.split("=")) // Split each entry into [key, encodedValue]
+//   .map((kv) => [kv[0].trim(), decodeURIComponent(kv[1])]) // Remap [key, encodedValue] -> [trimmedKey, decodedValue]
+//   .reduce<Record<string, any>>((o, kv) => {
+//     // Accumulate list into a single object
+//     o[kv[0]] = kv[1];
+//     return o;
+//   }, {});
+
+// // Check if a Memberspace JWT-style access token is present in our cookies
+// const isUserLoggedIn = "_ms-access-token" in cookieObject;
 
 export default isUserLoggedIn;
--- src/site-tweaks/member-signup-button.ts
+++ src/site-tweaks/member-signup-button.ts
@@ -1,43 +1,45 @@
 import { memberSignupButtonQuotes } from "./member-signup-button-quotes";
 
-// import isUserLoggedIn from "./is-user-logged-in";
-const isUserLoggedIn = false; // Always show the buttons
+import isUserLoggedIn from "./is-user-logged-in";
 
 //
-// Site tweak: Process member signup buttons
+// Site tweak: Apply quotes to member signup buttons inlined in pages
 //
 
-function mutateMemberSignupButton(fragmentIdentifier: string, applyQuotes: boolean) {
+// Find all links ending with (`$`) the given fragment identifier
+const quoteButtonFragmentIdentifier = "#member_signup";
+
+const quoteButtonLinks = document.querySelectorAll<HTMLLinkElement>(
+  `a[href$="${quoteButtonFragmentIdentifier}"]`
+);
+
+// Apply "Become a member" button quotes to links
+const startingQuoteIndex = Math.floor(Math.random() * memberSignupButtonQuotes.length);
+
+quoteButtonLinks.forEach((linkElement, index) => {
+  // Remove fragment from link
+  linkElement.href = linkElement.href.replace(quoteButtonFragmentIdentifier, "");
+
+  // Replace content
+  const quoteIndex = (startingQuoteIndex + index) % memberSignupButtonQuotes.length;
+
+  linkElement.innerText = memberSignupButtonQuotes[quoteIndex];
+});
+
+//
+// Site tweak: Change primary (bottom-left/footer) member signup button to point to account management when member is signed in
+//
+
+if (isUserLoggedIn) {
   // Find all links ending with (`$`) the given fragment identifier
-  const buttonLinks = document.querySelectorAll<HTMLLinkElement>(
-    `a[href$="${fragmentIdentifier}"]`
+  const menberButtonFragmentIdentifier = "#primary_member_signup";
+
+  const memberButtonLinks = document.querySelectorAll<HTMLLinkElement>(
+    `a[href$="${menberButtonFragmentIdentifier}"]`
   );
 
-  if (!isUserLoggedIn) {
-    if (!applyQuotes) {
-      // Nothing left to do
-      return;
-    }
-
-    // Apply "Become a member" button quotes to links
-    const startingQuoteIndex = Math.floor(Math.random() * memberSignupButtonQuotes.length);
-
-    buttonLinks.forEach((linkElement, index) => {
-      // Remove fragment from link
-      linkElement.href = linkElement.href.replace(fragmentIdentifier, "");
-
-      // Replace content
-      const quoteIndex = (startingQuoteIndex + index) % memberSignupButtonQuotes.length;
-
-      linkElement.innerText = memberSignupButtonQuotes[quoteIndex];
-    });
-  } else {
-    // Remove "Become a member" button
-    buttonLinks.forEach((linkElement) => {
-      linkElement.remove();
-    });
-  }
+  memberButtonLinks.forEach((linkElement) => {
+    linkElement.href = "/?msopen=%2Fmember%2Fsign_in";
+    linkElement.innerText = "Manage account";
+  });
 }
-
-mutateMemberSignupButton("#member_signup", true);
-mutateMemberSignupButton("#member_signup_verbatim", false);
