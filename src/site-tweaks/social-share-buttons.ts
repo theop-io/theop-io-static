bd8a889cdeec6fbfc9e57fc7421804593dbcc1b4
Robin Giese <robin@grumpycorp.com>
2023-08-26 16:37:26 -0700

    SiteTweaks: Wrap social links in a span instead of div

--- src/site-tweaks/social-share-buttons.ts
+++ src/site-tweaks/social-share-buttons.ts
@@ -1,30 +1,34 @@
+function buildShareToSocialLinksElement(): HTMLElement {
+  const socialSharingContainer = document.createElement("span");
+  socialSharingContainer.classList.add("shareon"); // for CSS
+
+  const addSharingLink = (service: string, href: string) => {
+    const sharingLink = document.createElement("a");
+    sharingLink.classList.add(service); // for CSS
+
+    sharingLink.href = href;
+    sharingLink.rel = "noopener noreferrer";
+    sharingLink.target = "_blank";
+
+    socialSharingContainer.appendChild(sharingLink);
+  };
+
+  const url = window.location.href;
+  const title = document.title;
+
+  addSharingLink("facebook", `https://www.facebook.com/sharer/sharer.php?u=${url}`);
+  addSharingLink("twitter", `https://twitter.com/intent/tweet?url=${url}&text=${title}`);
+  addSharingLink("whatsapp", `https://wa.me/?text=${title}%0D%0A${url}`);
+
+  return socialSharingContainer;
+}
+
 {
   const headerActionsDiv = document.querySelector<HTMLDivElement>(
     ".header-actions-action.header-actions-action--cta"
   );
 
   if (headerActionsDiv) {
-    const socialSharingDiv = document.createElement("div");
-    socialSharingDiv.classList.add("shareon"); // Identifies this as the target for @shareon
-
-    const addSharingLink = (service: string, href: string) => {
-      const sharingLink = document.createElement("a");
-      sharingLink.classList.add(service); // for CSS
-
-      sharingLink.href = href;
-      sharingLink.rel = "noopener noreferrer";
-      sharingLink.target = "_blank";
-
-      socialSharingDiv.appendChild(sharingLink);
-    };
-
-    const url = window.location.href;
-    const title = document.title;
-
-    addSharingLink("facebook", `https://www.facebook.com/sharer/sharer.php?u=${url}`);
-    addSharingLink("twitter", `https://twitter.com/intent/tweet?url=${url}&text=${title}`);
-    addSharingLink("whatsapp", `https://wa.me/?text=${title}%0D%0A${url}`);
-
-    headerActionsDiv.insertBefore(socialSharingDiv, headerActionsDiv.firstChild);
+    headerActionsDiv.insertBefore(buildShareToSocialLinksElement(), headerActionsDiv.firstChild);
   }
 }
